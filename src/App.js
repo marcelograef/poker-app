b2fa870cf3245a882824f1beacc845a90ba9a8f5
Marcelo Alejandro Graef <m.graef@ptytechnologies.com>
2022-11-11 22:46:29 -0300

    chore: test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* src/App.js */
+import { Button, Heading, withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import React, { useState } from 'react';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const initialState = { name: '', description: '' };
+
+const App = ({ signOut, user }) => {
+	const [formState, setFormState] = useState(initialState);
+
+	function setInput(key, value) {
+		setFormState({ ...formState, [key]: value });
+	}
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Hello from V2</h1>
-			</header>
+		<div style={styles.container}>
+			<Heading level={1}>Hello {user.username}</Heading>
+			<Button onClick={signOut} style={styles.button}>
+				Sign out
+			</Button>
+			<h2>Amplify Todos</h2>
+			<input
+				onChange={event => setInput('name', event.target.value)}
+				style={styles.input}
+				value={formState.name}
+				placeholder="Name"
+			/>
+			<input
+				onChange={event => setInput('description', event.target.value)}
+				style={styles.input}
+				value={formState.description}
+				placeholder="Description"
+			/>
 		</div>
 	);
-}
+};
 
-export default App;
+const styles = {
+	container: {
+		width: 400,
+		margin: '0 auto',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		padding: 20
+	},
+	todo: { marginBottom: 15 },
+	input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+	todoName: { fontSize: 20, fontWeight: 'bold' },
+	todoDescription: { marginBottom: 0 },
+	button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
+};
+
+export default withAuthenticator(App);
